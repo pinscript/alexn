32251d3584c9ce5444567b6157ae44bd01de568d
alexn <nyquist.alexander@gmail.com>
2011-06-17 09:26:58 +0200

    Removed PriorityQueue

--- alexn/Eventbus/IEventDispatcher.cs
+++ alexn/Eventbus/IEventDispatcher.cs
@@ -1,31 +1,7 @@
-using System;
-using System.Linq;
-using System.Reflection;
-
 namespace alexn.Eventbus
 {
     public interface IEventDispatcher
     {
         void Dispatch<TEvent>(TEvent @event) where TEvent : DomainEvent;
-    }
-
-    public class StaticEventDispatcher : IEventDispatcher {
-        private readonly Assembly[] _assemblies;
-
-        public StaticEventDispatcher(params Assembly[] assemblies) {
-            _assemblies = assemblies;
-        }
-
-        public void Dispatch<TEvent>(TEvent @event) where TEvent : DomainEvent {
-            foreach(var assembly in _assemblies) {
-                var handlers = assembly.GetTypes().Where(x => typeof(IHandle<TEvent>).IsAssignableFrom(x));
-
-                foreach (var handler in handlers) {
-                    var instance = Activator.CreateInstance(handler) as IHandle<TEvent>;
-                    if (instance != null)
-                        instance.Handle(@event);
-                }
-            }
-        }
     }
 }
---
+++ alexn/Eventbus/StaticEventDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace alexn.Eventbus {
+    public class StaticEventDispatcher : IEventDispatcher {
+        private readonly Assembly[] _assemblies;
+
+        public StaticEventDispatcher(params Assembly[] assemblies) {
+            _assemblies = assemblies;
+        }
+
+        public void Dispatch<TEvent>(TEvent @event) where TEvent : DomainEvent {
+            foreach(var assembly in _assemblies) {
+                var handlers = assembly.GetTypes().Where(x => typeof(IHandle<TEvent>).IsAssignableFrom(x));
+
+                foreach (var handler in handlers) {
+                    var instance = Activator.CreateInstance(handler) as IHandle<TEvent>;
+                    if (instance != null)
+                        instance.Handle(@event);
+                }
+            }
+        }
+    }
+}
